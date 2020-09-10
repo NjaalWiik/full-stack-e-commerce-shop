c96139842bdf8ba9c37a30bf25eb8bf30de1e42a
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-10 12:50:03 +0200

    created components homepage, Directory and MenuItem. Some styling applied as well

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
 
+// Components
+import MenuItem from '../../components/directory/Directory';
+
 // Style
 import './HomePage.styles.scss';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>HATS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>JACKETS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>SNEAKERS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>WOMENS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>MENS</h1>
-            <span className='subtitle'>SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <MenuItem />
     </div>
   );
 };
