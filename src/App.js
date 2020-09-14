bd37d765da8e3f63194c7de69040b3cb834e6b2f
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-14 14:54:13 +0200

    Updating app so that redirect if user is still active

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
 import './App.css';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -12,7 +10,7 @@ import ShopPage from './pages/shop/ShopPage';
 import SignInSignUpPage from './pages/sign-in-sign-up/SignInSignUpPage';
 import { setCurrentUser } from './redux/user/user.actions';
 
-function App({ setCurrentUser }) {
+function App({ currentUser, setCurrentUser }) {
   useEffect(() => {
     let unsubscribe = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -38,14 +36,24 @@ function App({ setCurrentUser }) {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInSignUpPage} />
+        <Route
+          exact
+          path='/signin'
+          render={() =>
+            currentUser ? <Redirect to='/' /> : <SignInSignUpPage />
+          }
+        />
       </Switch>
     </div>
   );
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
