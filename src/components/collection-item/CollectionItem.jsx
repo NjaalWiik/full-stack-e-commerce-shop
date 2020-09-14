6a4525b02bf1c01da0e758793878451bf65c58b8
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-14 16:23:30 +0200

    Implemented new action ADD_ITEM

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/CustomButton';
+import { addItem } from '../../redux/cart/cart.actions';
 
 // Styles
 import './CollectionItem.styles.scss';
 
-const CollectionItem = ({ name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div
@@ -18,9 +22,13 @@ const CollectionItem = ({ name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton>Add to cart</CustomButton>
+      <CustomButton onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
