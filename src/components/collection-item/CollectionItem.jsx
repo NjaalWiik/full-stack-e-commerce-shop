95562da6e1d3887b4900a5d0e730981e375efb31
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-15 08:56:09 +0200

    updated shop data with unique ids, updated cart utils to group items in state

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -20,7 +20,7 @@ const CollectionItem = ({ item, addItem }) => {
       />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        <span className='price'>${price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
